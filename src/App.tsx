c782146a85880558b4cb7190ba4e930f52332c4e
camper0008 <tphollebeek@gmail.com>
2022-07-07 16:17:51 +0200

    animations and design

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import Grid from "./Grid"
+
+function App() {
+  return (
+    <div className="App">
+        <Grid />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Grid.tsx
@@ -0,0 +1,15 @@
+import './Grid.scss'
+import House from "./House"
+
+function Grid() {
+  return (
+    <div className="grid">
+        <House bought />
+        <House />
+        <House />
+        <House />
+    </div>
+  )
+}
+
+export default Grid
